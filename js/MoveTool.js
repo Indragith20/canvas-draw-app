12d0bf1451018b11ac695f9662b1840bd073bb49
Indragith <indragith20@gmail.com>
2022-01-27 21:15:34 +0530

    Moving line around canvas implementation

--- js/MoveTool.js
+++ js/MoveTool.js
@@ -43,17 +43,34 @@ class MoveTool {
           endY: e._y
         });
       } else if (this.element.type === 'arrow') {
+        let endX = this.element.endX > this.element.x ? e._x + this.element.width : e._x - this.element.width;
+        let endY = this.element.endY > this.element.y ? e._y + this.element.height : e._y - this.element.height;
         this.callback({
           id: this.id,
           type: 'arrow',
           x: e._x,
           y: e._y,
-          startX: e._x, // for drawing rectangle around arrow. Deletion purpose
-          startY: e._y,// for drawing rectangle around arrow. Deletion purpose
+          startX: Math.min(endX, e._x), // for drawing rectangle around arrow. Deletion purpose
+          startY: Math.min(endY, e._y),// for drawing rectangle around arrow. Deletion purpose
           width: this.element.width,
           height: this.element.height,
-          endX: this.element.endX > this.element.x ? e._x + this.element.width : e._x - this.element.width,
-          endY: this.element.endY > this.element.y ? e._y + this.element.height : e._y - this.element.height
+          endX,
+          endY
+        });
+      } else if (this.element.type === 'line') {
+        let endX = this.element.endX > this.element.x ? e._x + this.element.width : e._x - this.element.width;
+        let endY = this.element.endY > this.element.y ? e._y + this.element.height : e._y - this.element.height;
+        this.callback({
+          id: this.id,
+          type: 'line',
+          x: e._x,
+          y: e._y,
+          startX: Math.min(e._x, endX), // for drawing rectangle around arrow. Deletion purpose
+          startY: Math.min(e._y, endY),// for drawing rectangle around arrow. Deletion purpose
+          width: this.element.width, // Check if it is wrong calculation.
+          height: this.element.height, //Check if it is wrong calculation
+          endX,
+          endY
         });
       }
 
@@ -102,6 +119,26 @@ class MoveTool {
       this.tempContext.lineTo(endX - headlen * Math.cos(angle + Math.PI / 6), endY - headlen * Math.sin(angle + Math.PI / 6));
       this.tempContext.stroke();
       this.tempContext.closePath();
+    } else if (this.element.type === 'line') {
+      this.tempContext.clearRect(0, 0, this.tempCanvas.width, this.tempCanvas.height);
+      let endX;
+      let endY;
+      if (this.element.endX > this.element.x) {
+        endX = e._x + this.element.width;
+      } else {
+        endX = e._x - this.element.width;
+      }
+
+      if (this.element.endY > this.element.y) {
+        endY = e._y + this.element.height;
+      } else {
+        endY = e._y - this.element.height;
+      }
+      this.tempContext.beginPath();
+      this.tempContext.moveTo(e._x, e._y);
+      this.tempContext.lineTo(endX, endY);
+      this.tempContext.stroke();
+      this.tempContext.closePath();
     }
   }
 
@@ -123,6 +160,12 @@ class MoveTool {
       this.tempContext.lineTo(this.element.endX - headlen * Math.cos(angle - Math.PI / 6), this.element.endY - headlen * Math.sin(angle - Math.PI / 6));
       this.tempContext.moveTo(this.element.endX, this.element.endY);
       this.tempContext.lineTo(this.element.endX - headlen * Math.cos(angle + Math.PI / 6), this.element.endY - headlen * Math.sin(angle + Math.PI / 6));
+      this.tempContext.stroke();
+      this.tempContext.closePath();
+    } else if (this.element.type === 'line') {
+      this.tempContext.beginPath();
+      this.tempContext.moveTo(this.element.x, this.element.y);
+      this.tempContext.lineTo(this.element.endX, this.element.endY);
       this.tempContext.stroke();
       this.tempContext.closePath();
     }
