b518883a1df6bd273edde0a6dca88363f95572b0
indragith.m <indragith.m@zohocorp.com>
2022-06-03 15:55:37 +0530

    heroku setup

--- remix.config.js
+++ remix.config.js
@@ -2,19 +2,24 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
+  // // appDirectory: "app",
+  // // assetsBuildDirectory: "public/build",
+  // // serverBuildPath: "build/index.js",
+  // // publicPath: "/build/",
+  // // devServerPort: 8002
+  // serverBuildTarget: "vercel",
+  // // When running locally in development mode, we use the built in remix
+  // // server. This does not understand the vercel lambda module format,
+  // // so we default back to the standard build output.
+  // server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  // ignoredRouteFiles: ["**/.*"],
+  // // appDirectory: "app",
+  // // assetsBuildDirectory: "public/build",
+  // // serverBuildPath: "api/index.js",
+  // // publicPath: "/build/",
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002
 };
