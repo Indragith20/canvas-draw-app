d93e1f23840bd01bd16fb5abfe614b9a9149bf57
Indragith20 <indragith20@gmail.com>
2022-10-03 19:24:05 +0530

    Added Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ["@remix-run/eslint-config"],
+  plugins: [
+    "react-hooks"
+  ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
