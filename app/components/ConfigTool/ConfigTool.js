05b925b4d5d5a497a3e04ae5422d2706fc8310a5
Indragith20 <indragith20@gmail.com>
2022-11-29 17:33:02 +0530

    Popover Implementation

--- app/components/ConfigTool/ConfigTool.js
+++ app/components/ConfigTool/ConfigTool.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Collaborators from '../Collaborators/Collaborators';
 import { useMatchMedia } from '../Common/hooks/useMatchMedia';
 import ThemeSwitcher from '../MainHeader/ThemeSwitcher';
 import styles from './ConfigTool.css';
@@ -31,7 +32,9 @@ function ConfigTool({ downloadImage, deleteCanvas, shareLink }) {
         <div className='icon' onClick={shareLink}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
         </div>
-
+        <div className='icon'>
+          <Collaborators />
+        </div>
         <div>
           <ThemeSwitcher />
         </div>
--- app/components/SingleRoom/SingleRoom.js
+++ app/components/SingleRoom/SingleRoom.js
@@ -1,5 +1,6 @@
 import { Link } from '@remix-run/react';
-import React from 'react';
+import React, { useState } from 'react';
+import PopOver from '../Common/Popover/PopOver';
 import { formatDate } from '../utils/formatDate';
 import styles from './SingleRoom.css';
 
@@ -12,8 +13,27 @@ export default function SingleRoom({ id, roomName, createdAt, createdBy }) {
   return (
 
     <div className='room'>
-      <div>
+      <div className='additional-actions'>
         {/* Created At {createdAt} */}
+
+        <PopOver position='right'>
+          <PopOver.TriggerNode>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
+          </PopOver.TriggerNode>
+          <PopOver.Content>
+            <ul className='actions-list'>
+              <li className='actions-list-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                <span>View Collaborators</span>
+              </li>
+              <li className='actions-list-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                <span>Delete</span>
+              </li>
+            </ul>
+          </PopOver.Content>
+
+        </PopOver>
       </div>
       <div className='room-name'>
         <Link to={`/draw/${id}`} className='room-link' replace={true}>
