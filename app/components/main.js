cdd5e6527f49ed449507727f38a69d4b82d126bf
Indragith20 <indragith20@gmail.com>
2023-01-10 13:27:18 +0530

    PageX/PageY test

--- app/components/main.js
+++ app/components/main.js
@@ -312,8 +312,8 @@ class MainComponent extends React.PureComponent {
       //   }
 
       // }
-      ev.x = ev.targetTouches[0].clientX;
-      ev.y = ev.targetTouches[0].clientY;
+      ev.x = ev.targetTouches[0].pageX;
+      ev.y = ev.targetTouches[0].pageY;
       this.onEvent(ev);
     } else {
       this.onEvent(ev);
@@ -342,8 +342,8 @@ class MainComponent extends React.PureComponent {
     console.log('Touch Move Fired', ev);
     let { selectedTool } = this.state;
     if (ev.targetTouches.length === 1) {
-      let deltaX = this.touchStartX - ev.targetTouches[0].clientX;
-      let deltaY = this.touchStartY - ev.targetTouches[0].clientY;
+      let deltaX = this.touchStartX - ev.targetTouches[0].pageX;
+      let deltaY = this.touchStartY - ev.targetTouches[0].pageY;
       let thresholdedDelta = this.onTouchMoveThreshold(deltaX, deltaY);
       ev.deltaX = thresholdedDelta.x;
       ev.deltaY = thresholdedDelta.y;
@@ -351,8 +351,8 @@ class MainComponent extends React.PureComponent {
       if (selectedTool === 'select') {
         this.onWheelMove(ev);
       } else {
-        ev.x = ev.targetTouches[0].clientX;
-        ev.y = ev.targetTouches[0].clientY;
+        ev.x = ev.targetTouches[0].pageX;
+        ev.y = ev.targetTouches[0].pageY;
         this.onEvent(ev);
       }
 
@@ -368,15 +368,15 @@ class MainComponent extends React.PureComponent {
         this.touchStartTimer = setTimeout(() => {
           this.touchStartTimer = null;
         }, this.DELTA_TIME_THRESHOLD_MS);
-        this.touchStartX = ev.changedTouches[0].clientX;
-        this.touchStartY = ev.changedTouches[0].clientY;
-        ev.x = ev.changedTouches[0].clientX;
-        ev.y = ev.changedTouches[0].clientY;
+        this.touchStartX = ev.changedTouches[0].pageX;
+        this.touchStartY = ev.changedTouches[0].pageY;
+        ev.x = ev.changedTouches[0].pageX;
+        ev.y = ev.changedTouches[0].pageY;
         this.onEvent(ev);
       } else {
-        if ((Math.abs(ev.changedTouches[0].clientX - this.touchStartX) < 10) && (Math.abs(ev.changedTouches[0].clientY - this.touchStartY) < 10)) {
-          ev.x = ev.changedTouches[0].clientX;
-          ev.y = ev.changedTouches[0].clientY;
+        if ((Math.abs(ev.changedTouches[0].pageX - this.touchStartX) < 10) && (Math.abs(ev.changedTouches[0].pageY - this.touchStartY) < 10)) {
+          ev.x = ev.changedTouches[0].pageX;
+          ev.y = ev.changedTouches[0].pageY;
           this.changeToTextTool(ev);
         }
       }
